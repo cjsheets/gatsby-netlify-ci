e51a26e8eea5cd0613dbc7a843269f96439908fa
Chad Sheets <cjsheets@gmail.com>
2021-09-18 14:32:18 -0700

    add playwright pacakges

---
+++ e2e/landing-page.e2e.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { test: originalTest, expect } = require('@playwright/test');
+
+const test = originalTest.extend({
+  url: 'http://localhost:8080',
+});
+
+const { describe, beforeEach } = test;
+
+describe('Landing Page', () => {
+  beforeEach(async ({ page, url }) => {
+    await page.goto(url);
+  });
+
+  test('displays the correct title', async ({ page }) => {
+    const title = await page.title();
+
+    expect(title).toBe('Gatsby Netlify CI');
+  });
+});
